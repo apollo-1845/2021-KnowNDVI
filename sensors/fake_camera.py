2d2cd18466d5f2fd9f66b8a4811b86d61e7df2b6
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2022-01-22 18:37:54 +0000

    Made FakeCamera more flexible - #7

--- sensors/fake_camera.py
+++ sensors/fake_camera.py
@@ -6,20 +6,31 @@ from sensors.sensor import Sensor
 
 
 class FakeCamera(Sensor):
-    url = None
+    image = None
 
-    def set_image(self, id):
+    def set_id(self, id:int):
         """Set the input image to a certain number id of the images at https://github.com/raspberrypilearning/astropi-ndvi/tree/master/en/resources"""
-        self.url = f"https://github.com/raspberrypilearning/astropi-ndvi/blob/master/en/resources/cslab3ogel_Files_RawData_raw_image_{id}.jpeg?raw=true"
+        # Get data link
+        url = f"https://github.com/raspberrypilearning/astropi-ndvi/blob/master/en/resources/cslab3ogel_Files_RawData_raw_image_{id}.jpeg?raw=true"
+        self.set_url(url)
+
+    def set_local(self, path:str):
+        with open(path, "rb") as file_bytes:
+            # Set input image
+            self.set_bytes(file_bytes)
+
+    def set_url(self, url:str):
+        """Set the input image to the data from a certain URL"""
+        # Request data from dataset
+        with requests.get(url) as r:
+            byte_resp = r.content
+            # Set input image
+            self.set_bytes(byte_resp)
+
+    def set_bytes(self, bytes_in:bytes):
+        array_resp = numpy.array(list(bytes_in), numpy.uint8)
+        # Convert to OpenCV Image
+        self.image = cv2.imdecode(array_resp, cv2.IMREAD_UNCHANGED)
 
     def capture_data(self):
-        # Get data link
-        url = self.url
-        # Request data from dataset
-        r = requests.get(url)
-        byte_resp = r.content
-        array_resp = numpy.array(list(byte_resp), numpy.uint8)
-        r.close()
-        # Convert to OpenCV Image
-        image = cv2.imdecode(array_resp, cv2.IMREAD_UNCHANGED)
-        return image
+        return self.image
