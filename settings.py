7bfd69378e792cf7660b6223ec34531f39b3a8b6
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2022-02-24 16:31:42 +0000

    Add preferred paths

--- settings.py
+++ settings.py
@@ -7,8 +7,9 @@ EXPERIMENT_DURATION_MINUTES = 3 * 60
 SECONDS_PER_ITERATION = 1
 
 # Processing
-PREFERRED_RESOLUTION = (640, 480)
-PREFERRED_RES_NP = (480, 640)  # Reverse - for creating NumPy arrays
+# Resolution from phase 2 guide
+PREFERRED_RESOLUTION = (1296, 972)
+PREFERRED_RES_NP = (972, 1296)  # Reverse - for creating NumPy arrays
 
 MASK = True
 CAN_DISCARD = True
@@ -21,12 +22,19 @@ CAN_DISCARD = True
 # Discard, no PNG - 26M
 # Discard, PNG - 23M
 #
-# 10 pictures take up 2.2M
-# We are allowed to use 3G = 2600M (conservatively)
-# Roughly 12000 pictures are allowed
-# 3 hours = 10800 seconds, therefore 1 picture per second should work well
+# With 480x640:
+    # 10 pictures take up around 2.2M
+    # We are allowed to use 3G = 2600M (conservatively)
+    # Roughly 12000 pictures are allowed
+    # 3 hours = 10800 seconds, therefore 1 picture per second should work well
+# Now:
+# 1 picture per 5 seconds
 USE_PNG = True
 
 # Output
-OUT_DIR = os.path.join(".", "out")
-OUT_FILE = os.path.join(OUT_DIR, "out.blob")
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.resolve() # Get parent directory
+
+OUT_DIR = BASE_DIR/"out"
+OUT_FILE = OUT_DIR/"out.blob"
